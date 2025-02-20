82188f9e67505c3949a0006fc346b9c598a02d3b
Sanjar <usalmazovich@gmail.com>
2025-02-21 05:00:46 +0600

    S

--- chat/scripts/search.js
+++ chat/scripts/search.js
@@ -3,6 +3,8 @@ import {renderSearchedContacts} from './contact.js';
 const inputElement = document.querySelector('.js-input');
 
 inputElement.addEventListener('keydown', (event) => {
-    let name = inputElement.value;
-    renderSearchedContacts(name)
+    if (event.key === 'Enter') {
+        let name = inputElement.value;
+        renderSearchedContacts(name)
+    };
 });
